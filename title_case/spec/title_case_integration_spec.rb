a1bb2399a76d2fc0a5d61363136fb4138f799ee8
James Turello and Matt Wilkin <james@>
2016-06-29 13:11:12 -0700

    added more tests.

--- title_case/spec/title_case_integration_spec.rb
+++ title_case/spec/title_case_integration_spec.rb
@@ -8,6 +8,21 @@ describe('the title case path', {:type => :feature}) do
     visit('/')
     fill_in('title', :with => 'green eggs and ham')
     click_button('Send')
-    expect(page).to have_content('Green Eggs and Ham')
+    expect(page).to have_content('Green Eggs And Ham')
   end
+
+  it('does not modify non-letter input') do
+    visit('/')
+    fill_in('title', :with => '123 123 @this')
+    click_button('Send')
+    expect(page).to have_content('123 123 @this')
+  end
+
+  it('empty string input returns an empty string and does not fail') do
+    visit('/')
+    fill_in('title', :with => '')
+    click_button('Send')
+    expect(page).to have_content('')
+  end
+
 end
